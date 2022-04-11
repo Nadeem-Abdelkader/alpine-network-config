b5d7fba0cf3e89cb571abe3e4612c702281e868e
nabd0018 <nabd0018@student.monash.edu>
2022-04-11 22:47:47 +0200

    updated main.py & functions.py

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# Alpine Network Configuration Application
+# Alpine Boot Application
 
 ## Author
 - [Nadeem Abdelkader](https://github.com/Nadeem-Abdelkader)
 
 ## What Is This?
-A Python Tkinter form that reads network configuration data from multiple txt files and allows user to edit the data then rewrites the updated data to the txt files.
+A Python Tkinter form that reads data from a txt file and allows user to edit the data then rewrites the updated data to the txt file.
 
 ## Set Up and Run
 
 1. Download and install Python 3.10 from <https://www.python.org/downloads/> and make sure to add Python to PATH if you are using Windows
 2. Clone or download the git repository
-   [here](https://github.com/Nadeem-Abdelkader/alpine-network-config).
+   [here](https://github.com/Nadeem-Abdelkader/alpine-boot).
     ```sh
-    git clone https://github.com/Nadeem-Abdelkader/alpine-network-config
+    git clone https://github.com/Nadeem-Abdelkader/alpine-boot
     ```
 3. Navigate to the cloned local repository
     ```sh
-    cd alpine-network-config
+    cd alpine-boot
     ```
 
 4. To start the application, run the following command while inside the cloned local repository:
--- functions.py
+++ functions.py
@@ -17,16 +17,108 @@ import os
 from tkinter import Frame, Label, Entry, X, LEFT, RIGHT, YES, messagebox, Button, Tk, TOP
 
 # declaring the constants to be used everywhere in the module
-FIELDS = ('iface', 'inet', 'address', 'netmask', 'gateway', 'DNSOPTS', 'HOSTNAMEOPTS')
+FIELDS_1 = ['KEYMAPOPTS', 'HOSTNAMEOPTS', 'INTERFACESOPTS', 'DNSOPTS', 'TIMEZONEOPTS', 'PROXYOPTS',
+            'APKREPOSOPTS', 'SSHDOPTS', 'NTPOPTS', 'DISKOPTS', 'LBUOPTS', 'APKCACHEOPTS']
 
-DISPlAY_FIELDS = ('Interface', 'Internet Networking', 'IP address', 'Netmask', 'Gateway', 'DNS and Domain', 'Host Name')
+FIELDS_2 = ['iface', 'inet', 'address', 'netmask', 'gateway', 'domain', 'nameserver', 'hostname']
 
-USERS_FILENAME = "output.txt"
+DISPlAY_FIELDS = ['Interface', 'Internet Networking', 'IP address', 'Netmask', 'Gateway', 'Domain', 'Name Servers',
+                  'Host Name']
+
+INPUT_DIR = "/Users/nadeem/Documents/Khwarizm/Alpine/alpine-network-config/input_dir/"
+OUTPUT_DIR = "/Users/nadeem/Documents/Khwarizm/Alpine/alpine-network-config/output_dir/"
+
+data_dict_1 = {
+    FIELDS_1[0]: None,
+    FIELDS_1[1]: None,
+    FIELDS_1[2]: None,
+    FIELDS_1[3]: None,
+    FIELDS_1[4]: None,
+    FIELDS_1[5]: None,
+    FIELDS_1[6]: None,
+    FIELDS_1[7]: None,
+    FIELDS_1[8]: None,
+    FIELDS_1[9]: None,
+    FIELDS_1[10]: None,
+    FIELDS_1[11]: None,
+}
+
+data_dict_2 = {
+    FIELDS_2[0]: None,
+    FIELDS_2[1]: None,
+    FIELDS_2[2]: None,
+    FIELDS_2[3]: None,
+    FIELDS_2[4]: None,
+    FIELDS_2[5]: None,
+    FIELDS_2[6]: [],
+    FIELDS_2[7]: None
+}
+
+
+def read_to_dict_1(filename):
+    """
+    This function reads data from a txt file to a dictionary
+    :param filename: file to read from
+    :return: dictionary contining read data
+    """
+    f = open(filename, 'r')
+    f = f.read()
+    f = f.split('\"')
+    for i in range(len(f)):
+        for j in range(len(FIELDS_1)):
+            if f[i].endswith(FIELDS_1[j] + "="):
+                data_dict_1[FIELDS_1[j]] = f[i + 1]
+    return data_dict_1
+
+
+def read_to_dict_2():
+    """
+    This function reads data from a txt file to a dictionary
+    :param filename: file to read from
+    :return: dictionary contining read data
+    """
+    filename = "input_dir/interfaces.txt"
+    if filename == "input_dir/interfaces.txt":
+        f = open(filename, 'r')
+        f = f.read()
+        f = f.replace('\n', ' ')
+        f = f.split(' ')
+        # print(f)
+        for i in range(len(f)):
+            for j in range(len(FIELDS_2)):
+                if f[i] == FIELDS_2[j]:
+                    data_dict_2[FIELDS_2[j]] = f[i + 1]
+    filename = "input_dir/resolve.txt"
+    if filename == "input_dir/resolve.txt":
+        f = open(filename, 'r')
+        f = f.read()
+        f = f.replace('\n', ' ')
+        f = f.split(' ')
+        f = list(filter(None, f))
+        f = list(filter(bool, f))
+        f = list(filter(len, f))
+        f = list(filter(lambda item: item, f))
+        # print(f)
+        for i in range(len(f)):
+            for j in range(len(FIELDS_2)):
+                if f[i] == FIELDS_2[j]:
+                    if FIELDS_2[j] == 'nameserver':
+                        data_dict_2[FIELDS_2[j]].append(str(f[i + 1]))
+                    else:
+                        data_dict_2[FIELDS_2[j]] = f[i + 1]
+    # TEMPORARY
+    data_dict_2['hostname'] = "myhost"
+    return data_dict_2
+
+
+# data1 = read_to_dict_1("answers.txt")
+# print(data1)
+
+# data2 = read_to_dict_2()
+# print(data2)
 
 
 def read_txt_to_lst(filename):
-    return
-    """TODO: UPDATE THIS FUNCTION FOR NETWORK CONFIGURATION DATA FROM MULTIPLE TXT FILES"""
     """
     This function reads the data from .txt file to a list
     :param filename: txt file to read from
@@ -39,10 +131,13 @@ def read_txt_to_lst(filename):
     with open(filename, 'rt') as myfile:
         for myline in myfile:
             # print(myline)
-            if myline.startswith(FIELDS[0]) or myline.startswith(FIELDS[1]) or myline.startswith(
-                    FIELDS[2]) or \
-                    myline.startswith(FIELDS[3]) or myline.startswith(FIELDS[4]) or myline.startswith(FIELDS[5]) \
-                    or myline.startswith(FIELDS[6]):
+            if myline.startswith(FIELDS_2[0]) or myline.startswith(FIELDS_2[1]) or myline.startswith(
+                    FIELDS_2[2]) or \
+                    myline.startswith(FIELDS_2[3]) or myline.startswith(FIELDS_2[4]) or myline.startswith(FIELDS_2[5]) \
+                    or myline.startswith(FIELDS_2[6]) or myline.startswith(FIELDS_2[7]) or \
+                    myline.startswith(FIELDS_2[8]) or myline.startswith(FIELDS_2[9]) or \
+                    myline.startswith(FIELDS_2[10]) or \
+                    myline.startswith(FIELDS_2[11]):
                 start = myline.find("\"")
                 end = myline.rfind("\"")
                 # print(myline[start+1:end])
@@ -63,8 +158,9 @@ def make_form(root, fields):
     makeLabel(root)
     entries = {}
     i = 0
-    data = read_txt_to_lst("answers.txt")
-    data = []
+    # data = read_txt_to_lst("answers.txt")
+    data = read_to_dict_2()
+    # print(data)
     for field in fields:
         row = Frame(root)
         lab = Label(row, width=22, text=DISPlAY_FIELDS[i] + ": ", anchor='w')
@@ -72,8 +168,19 @@ def make_form(root, fields):
             ent = Entry(row, show="*")
         else:
             ent = Entry(row)
-            if i < len(data):
-                ent.insert(0, data[i])
+            if type(data[field]) != list and data[field].startswith("-"):
+                # print(data[field][3:])
+                ent.insert(0, data[field][3:])
+            else:
+                if type(data[field]) == list:
+                    for j in data[field]:
+                        # print(data[field])
+                        if j == data[field][-1]:
+                            ent.insert(0, j + ", ")
+                        else:
+                            ent.insert(0, j)
+                else:
+                    ent.insert(0, data[field])
         ent.insert(0, "")
         row.pack(side=TOP, fill=X, padx=25, pady=5)
         lab.pack(side=LEFT)
@@ -84,18 +191,19 @@ def make_form(root, fields):
 
 
 def read(ents):
-    """TODO: UPDATE THIS FUNCTION FOR NETWORK CONFIGURATION DATA FROM MULTIPLE TXT FILES"""
     """
     This function re reads data from .txt file and re populates the entries
     :param ents: entries to re populate
     :return: void
     """
-    data = read_txt_to_lst("answers.txt")
-    data = []
-    for i in range(len(FIELDS)):
-        ents[FIELDS[i]].delete(0, 'end')
-        if i < len(data):
-            ents[FIELDS[i]].insert(0, data[i])
+    # data = read_txt_to_lst("answers.txt")
+    data = read_to_dict_1("input_dir/answers.txt")
+    for i in range(len(FIELDS_2)):
+        ents[FIELDS_2[i]].delete(0, 'end')
+        # if data[FIELDS[i]].startswith("-"):
+        #     ents[FIELDS[i]].insert(0, data[FIELDS[i]][3:])
+        # else:
+        #     ents[FIELDS[i]].insert(0, data[FIELDS[i]])
     txt_result.config(text="Successfully read data!", fg="green")
     return
 
@@ -139,7 +247,6 @@ def makeLabel(root):
 
 
 def submit(entries):
-    """TODO: UPDATE THIS FUNCTION FOR NETWORK CONFIGURATION DATA FROM MULTIPLE TXT FILES"""
     """
     This function is executed when the user fills in all the inforamtion and clicks submit.
     It takes all the entered information an writes it to a .json file (users.json)
@@ -159,7 +266,7 @@ def submit(entries):
     if cont:
         dict = {}
         for i in range(len(entries)):
-            dict[FIELDS[i]] = entries[FIELDS[i]].get()
+            dict[FIELDS_2[i]] = entries[FIELDS_2[i]].get()
         #
         # users_list = []
         # if os.path.exists(USERS_FILENAME) and os.stat(USERS_FILENAME).st_size != 0:
@@ -170,48 +277,41 @@ def submit(entries):
 
         # print(dict)
 
-        filename = str(entries[FIELDS[0]].get()).replace(" ", "") + ".json"
-        filename = "/Users/nadeem/Documents/Khwarizm/Alpine/alpine-network-config/records/" + filename
-        filename = USERS_FILENAME
+        # interfaces_file = str(entries[FIELDS_2[0]].get()).replace(" ", "") + ".json"
+        interfaces_file = OUTPUT_DIR + "interfaces.txt"
+        resolve_file = OUTPUT_DIR + "resolve.txt"
+        host_file = OUTPUT_DIR + "host.txt"
+
         # with open(filename,
         #           "w") as write_file:  # change "w" to "a" if you want to append instead of overwrite
         #     json.dump(dict, write_file, indent=4)
-        comments = ["# Example answer file for setup-alpine script\n"
-                    "# If you don't want to use a certain option, then comment it out\n\n"
-                    "# Use US layout with US variant\n",
-                    "\n# Set hostname to alpine-test\n",
-                    "\n# Contents of /etc/network/interfaces\n",
-                    "\n# Search domain of example.com, Google public nameserver\n",
-                    "\n# Set timezone to UTC\n",
-                    "\n# set http/ftp proxy\n",
-                    "\n# Add a random mirror\n",
-                    "\n# Install Openssh\n",
-                    "\n# Use openntpd\n",
-                    "\n# Use /dev/sda as a data disk\n",
-                    "\n# Setup in /media/sdb1\n"
-                    ]
-        with open(filename, 'w') as file:
-            for i in range(len(FIELDS)):
-                if i < len(comments):
-                    file.write(comments[i])
-                if i == 1:
-                    file.write(FIELDS[i] + "=\"-n " + dict[FIELDS[i]] + "\"")
-                elif i == 3:
-                    file.write(FIELDS[i] + "=\"-d " + dict[FIELDS[i]] + "\"")
-                elif i == 4:
-                    file.write(FIELDS[i] + "=\"-z " + dict[FIELDS[i]] + "\"")
-                elif i == 6:
-                    file.write(FIELDS[i] + "=\"-r" + dict[FIELDS[i]] + "\"")
-                elif i == 7:
-                    file.write(FIELDS[i] + "=\"-c " + dict[FIELDS[i]] + "\"")
-                elif i == 8:
-                    file.write(FIELDS[i] + "=\"-c " + dict[FIELDS[i]] + "\"")
-                elif i == 9:
-                    file.write(FIELDS[i] + "=\"-m " + dict[FIELDS[i]] + "\"")
+        interfaces_file = open(interfaces_file, 'w')
+        resolve_file = open(resolve_file, 'w')
+        host_file = open(host_file, 'w')
+        for i in range(len(FIELDS_2)):
+            if FIELDS_2[i] in ['iface' , 'inet', 'address', 'netmask', 'gateway']:
+                if i >= 1:
+                    interfaces_file.write(FIELDS_2[i] + " " + dict[FIELDS_2[i]] + "\n")
                 else:
-                    file.write(FIELDS[i] + "=\"" + dict[FIELDS[i]] + "\"")
-                file.write("\n")
-            file.write("\n")
+                    interfaces_file.write(FIELDS_2[i] + " " + dict[FIELDS_2[i]] + " ")
+            if FIELDS_2[i] in ['domain', 'nameserver']:
+                if FIELDS_2[i] == 'domain':
+                    resolve_file.write(FIELDS_2[i] + "           " + dict[FIELDS_2[i]] + "\n")
+                if FIELDS_2[i] == 'nameserver':
+                    dict[FIELDS_2[i]] = dict[FIELDS_2[i]].replace(" ", "")
+                    dict[FIELDS_2[i]] = dict[FIELDS_2[i]].split(",")
+                    dict[FIELDS_2[i]] = dict[FIELDS_2[i]][::-1]
+                    for j in dict[FIELDS_2[i]]:
+                        print(j)
+                        resolve_file.write(FIELDS_2[i] + "       " + j + "\n")
+            if FIELDS_2[i] == 'hostname':
+                host_file.write(FIELDS_2[i] + "           " + dict[FIELDS_2[i]] + "\n")
+
+
+        # interfaces_file.write("\n")
+        # resolve_file.write("\n")
+        # host_file.write("\n")
+
         # jsonString = json.dumps(users_list, indent=4)
         # jsonFile = open(USERS_FILENAME, "w")
         # jsonFile.write(jsonString)
@@ -258,8 +358,8 @@ def clear(entries, on_submit=False):
     :param entries: an array of entries to clear
     :return: void
     """
-    for i in range(len(FIELDS)):
-        entries[FIELDS[i]].delete(0, 'end')
+    for i in range(len(FIELDS_2)):
+        entries[FIELDS_2[i]].delete(0, 'end')
     if not on_submit:
         txt_result.config(text="Cleared form!", fg="green")
     return
@@ -315,7 +415,7 @@ def initialise_window():
     """
     global root, ents
     root = Tk()
-    ents = make_form(root, FIELDS)
+    ents = make_form(root, FIELDS_2)
     root.geometry("800x600")
     root.title("Khwarizm Consulting")
     root.bind('<Return>', (lambda event, e=ents: fetch(e)))
